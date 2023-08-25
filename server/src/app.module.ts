913f82b9824a4f7a4521b786d36f008794fdf331
ferenc.merics <ferenc.merics@bitriderz.com>
2023-08-25 10:44:02 +0200

    fix db config and install postgres driver deps

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,37 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { UserEntity } from './user/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
